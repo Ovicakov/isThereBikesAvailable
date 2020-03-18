e618cd4426d5409e4590e8a1b9d2c81c4c344762
Clément Héré <clement.here.tech@gmail.com>
2020-03-18 22:05:23 +0100

    css and updates api

--- src/components/GetData.js
+++ src/components/GetData.js
@@ -1,17 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import './GetData.scss'
 
 export default function GetData() {
-  const [isThereABike, setIsThereABike] = useState()
+  const [bikesAvailable, setBikesAvailable] = useState()
+  const [bikeStation, setBikeStation] = useState()
 
   useEffect(() => {
     axios.get('https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&q=madagascar&facet=name&facet=is_installed&facet=is_renting&facet=is_returning&facet=nom_arrondissement_communes&refine.name=Madagascar+-+Meuniers')
-      .then(response => setIsThereABike(response.data.records[0].fields.numbikesavailable))
+      .then(response => setBikesAvailable(response.data.records[0].fields.numbikesavailable))
+
+    axios.get('https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&q=madagascar&facet=name&facet=is_installed&facet=is_renting&facet=is_returning&facet=nom_arrondissement_communes&refine.name=Madagascar+-+Meuniers')
+      .then(response => setBikeStation(response.data.records[0].fields.name))
   })
 
   return (
-    <div>
-      <div>Il y a {isThereABike} vélos disponibles à la station de vélib Madagascar</div>
+    <div className="container">
+      <div className="yesOrNoContainer">
+        {bikesAvailable > 5 ? 'OUI' : 'NON'}
+      </div>
+      <div className="stationContainer">{bikesAvailable} vélos disponibles à la station {bikeStation}</div>
     </div>
   )
 }
